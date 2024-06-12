8b0c72c9c07be711f0b37d29c71c51ab20c1a650
yqlili <92971034+yqlili@users.noreply.github.com>
2024-06-12 22:46:34 +0200

    Create tests.sh

---
+++ project/tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the target directory to store data to an environment variable
+export TARGET_DIR=$(dirname "$(pwd)")
+
+echo "Target working directory set to: $TARGET_DIR"
+
+python3 test.py
